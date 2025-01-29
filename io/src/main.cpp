482c1346b8fdbea833d77baa5d04c03f9aabb168
Piotr <piotr.k.nogalski@gmail.com>
2025-01-29 15:52:01 +0100

    Testing git push with token

--- io/src/main.cpp
+++ io/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	float a,b,pole;
-
+// changes for push tests
 	std::cout << "Program will calculate area of rectangle plese provide lengh of side a and side b" << std::endl;
 	std::cin >> a >> b;
 	std::cout << std::endl;
