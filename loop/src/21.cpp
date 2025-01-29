9c76d3f130b516fd40ab7f198f00dd5f72a7cdf2
Piotr <piotr.k.nogalski@gmail.com>
2025-01-29 15:32:12 +0100

    Solved chapter 2 task 1.

--- loop/src/21.cpp
+++ loop/src/21.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+
+int main(){
+	int a,b,c;
+
+	std::cout << "Program will check if three provied straight lenghts can form a right triangle." << std::endl;
+	std::cin >> a >> b >> c;
+	
+	if ((a*a+b*b) == c*c)
+		{
+			std::cout << "This is right triagnle" << std::endl;
+		}
+		else
+		{
+			std::cout << "This is NOT right triagnle" << std::endl;
+		}
+
+	return 0;
+
+}
